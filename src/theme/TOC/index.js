c3f5283f048a1395b6741f4cc7442b6d41083d92
vehidtr <vehid.trtak@gmail.com>
2022-09-28 16:10:19 +0200

    Change styling of TOC

---
+++ src/theme/TOC/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TOC from '@theme-original/TOC'
+
+export default function TOCWrapper(props) {
+  return (
+    <>
+      <div className='ml-1.5 text-sm font-bold'>On this page</div>
+      <TOC {...props} />
+      <div className="grid grid-flow-col items-center text-red-500 text-sm font-semibold ml-1.5 justify-start border-0 border-t border-solid border-black pt-4">
+        <img src="/img/question-icon.svg" className="mr-1.5" />
+        Questions? Give us feedback
+      </div>
+    </>
+  )
+}
