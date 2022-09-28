1ee1fbe861c07315403f133d6eeb7803d99450d0
vehidtr <vehid.trtak@gmail.com>
2022-09-28 18:58:50 +0200

    Set the correct icons for toggling dark mode

--- src/theme/Navbar/MobileSidebar/PrimaryMenu/index.js
+++ src/theme/Navbar/MobileSidebar/PrimaryMenu/index.js
@@ -15,16 +15,20 @@ export default function PrimaryMenuWrapper(props) {
       <div className=" border-0 border-solid border-t border-neutral-300 grid grid-cols-2 pt-4 pb-3">
         <div className="pl-3 grid items-center">Switch theme</div>
         <div className="grid grid-flow-col gap-1 items-center ml-auto p-3 mr-4 rounded-full cursor-pointer hover:drop-shadow-md">
-          {colorMode === 'light' ? (
+          {colorMode === 'dark' ? (
             <>
               <img
-                src="/img/lightbulb.svg"
-                onClick={() => handleMode('dark')}
+                src="/img/icons/dark.svg"
+                onClick={() => handleMode('light')}
               />
             </>
           ) : (
             <>
-              <img src="/img/shield.svg" onClick={() => handleMode('light')} />
+              <img
+                className="invert"
+                src="/img/icons/light.svg"
+                onClick={() => handleMode('dark')}
+              />
             </>
           )}
         </div>
--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -4,7 +4,7 @@ import TOC from '@theme-original/TOC'
 export default function TOCWrapper(props) {
   return (
     <>
-      <div className='ml-1.5 text-sm font-bold dark:white'>On this page</div>
+      <div className="ml-1.5 text-sm font-bold dark:white">On this page</div>
       <TOC {...props} />
       <div className="grid grid-flow-col items-center text-red-500 text-sm font-semibold ml-1.5 justify-start border-0 border-t border-solid border-black pt-4">
         <img src="/img/question-icon.svg" className="mr-1.5" />
