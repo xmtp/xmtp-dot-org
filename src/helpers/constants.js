c580fa651fa605b3cbb67c10c34a27d2ec4a4b2b
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2022-09-28 13:06:47 -0700

    text tweaks

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,6 @@
 export const HEADER_DATA = [
   {
-    title: 'Concepts',
+    title: 'Development concepts',
     subtitle:
       'Learn about the protocol, architecture, encryption, authentication, and more',
     url: '/docs/dev-concepts/introduction',
@@ -14,7 +14,7 @@ export const HEADER_DATA = [
   },
   {
     title: 'Vision',
-    subtitle: 'Read the XMTP Litepaper public draft, roadmap, and case studies',
+    subtitle: 'Read the XMTP Litepaper public draft and learn about the XMTP roadmap',
     url: '/about/litepaper',
     icon: 'xmtp-icon.svg'
   },
