e20180ba9d1da366ebd789a8de0c67cab055e04d
vehidtr <vehid.trtak@gmail.com>
2022-09-29 21:10:01 +0200

    Hide scroll to top when view position less than 1000

--- src/components/ToggleButtons/index.js
+++ src/components/ToggleButtons/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useColorMode } from '@docusaurus/theme-common'
 
 export default function ToggleButtons() {
   const { colorMode, setColorMode } = useColorMode()
+  const scrollToTopBtnRef = useRef(null)
+  const darkModeBtnRef = useRef(null)
 
   const handleDarkMode = () => {
     if (colorMode === 'dark') setColorMode('light')
@@ -17,15 +19,38 @@ export default function ToggleButtons() {
     })
   }
 
+  useEffect(() => {
+    window.onscroll = function () {
+      scrollFunction()
+    }
+  }, [])
+
+  const scrollFunction = () => {
+    if (
+      document.body.scrollTop > 1000 ||
+      document.documentElement.scrollTop > 1000
+    ) {
+      scrollToTopBtnRef.current.style.display = 'grid'
+      darkModeBtnRef.current.style.transform = 'translateY(0)'
+      darkModeBtnRef.current.style.transition = 'all 0.5s ease'
+    } else {
+      scrollToTopBtnRef.current.style.display = 'none'
+      darkModeBtnRef.current.style.transform = 'translateY(3.25rem)'
+      darkModeBtnRef.current.style.transition = 'all 0.5s ease'
+    }
+  }
+
   return (
     <>
       <div
+        ref={darkModeBtnRef}
         onClick={handleDarkMode}
-        className="w-11 h-11 rounded-full bg-gray-900 grid justify-center items-center border-2 border-solid border-gray-500 fixed right-12 bottom-20 z-50 cursor-pointer"
+        className="w-11 h-11 rounded-full bg-gray-900 grid justify-center items-center border-2 border-solid border-gray-500 right-12 bottom-20 z-[60] cursor-pointer fixed"
       >
         <img src={`/img/icons/${colorMode}.svg`} />
       </div>
       <div
+        ref={scrollToTopBtnRef}
         onClick={handleScrollToTop}
         className="w-11 h-11 rounded-full bg-gray-900 grid justify-center items-center border-2 border-solid border-gray-500 fixed right-12 bottom-7 z-50 cursor-pointer"
       >
