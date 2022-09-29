75a6337aa411e2a609771fd7be9b6d3409e29fdd
vehidtr <vehid.trtak@gmail.com>
2022-09-29 02:47:41 +0200

    Improve sidebar colors and toc dark mode

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -6,7 +6,7 @@ export default function TOCWrapper(props) {
     <>
       <div className="ml-1.5 text-sm font-bold dark:white">On this page</div>
       <TOC {...props} />
-      <div className="grid grid-flow-col items-center text-red-500 text-sm font-semibold ml-1.5 justify-start border-0 border-t border-solid border-black pt-4">
+      <div className="grid grid-flow-col items-center text-red-500 text-sm font-semibold ml-1.5 justify-start border-0 border-t border-solid border-black dark:border-neutral-500 pt-4 toc-footer">
         <img src="/img/question-icon.svg" className="mr-1.5" />
         Questions? Give us feedback
       </div>
