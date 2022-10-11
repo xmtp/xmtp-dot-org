978a8b15250e560b949da6862d1d50da99f63a2f
Darick Dang <darickdang@gmail.com>
2022-10-10 20:37:57 -0700

    Basic type and background fixes

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -5,7 +5,7 @@ import ALink from '../../components/ALink'
 export default function TOCWrapper(props) {
   return (
     <>
-      <div className="ml-1.5 text-sm font-bold dark:white">On this page</div>
+      <div className="ml-1.5 mt-2 text-sm font-bold dark:white">On this page</div>
       <TOC {...props} />
       <ALink
         to="https://github.com/orgs/xmtp/discussions/categories/q-a"
