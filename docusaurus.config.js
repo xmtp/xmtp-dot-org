d67e04f0a93fef94ee622a20e349806aaaf22362
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2022-11-23 10:29:07 -0800

    testing site image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,6 +91,7 @@ const config = {
         defaultMode:'light',
         disableSwitch: false,
       },
+      image: 'img/xmtp-postmark.png', // for social preview image
       navbar: {
         title: '',
         logo: {
