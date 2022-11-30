b9d0c9b2bb93fc83a53dfd327a591d6483f7caf2
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2022-11-30 12:39:00 -0800

    fix feedback link

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -4,7 +4,7 @@ import ALink from '../ALink'
 const Feedback = ({url}) => {
   return (
     <ALink
-      to={url}
+      to="https://github.com/orgs/xmtp/discussions/categories/q-a"
       className="grid grid-flow-col items-center text-red-500 text-sm font-semibold ml-1.5 justify-start border-0 border-t border-solid border-black dark:border-neutral-500 pt-4 toc-footer"
     >
       <img src="/img/question-icon.svg" className="mr-1.5" />
