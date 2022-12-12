d7721110d956189a0a9c4c527d54cbadbe3e9a6c
Darick Dang <darickdang@gmail.com>
2022-12-12 10:45:21 -0800

    Design fixes

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -4,21 +4,28 @@ import useBaseUrl from '@docusaurus/useBaseUrl/';
 
 const BuiltWithXmtp = () => {
   return (
-    <ul className="list-none grid grid-cols-1 md:grid-cols-2 gap-4 px-0">
+    <ul className="list-none grid grid-cols-1 md:grid-cols-2 gap-6 px-0">
       {ListOfDevelopers.map(developer => 
-        <li className="flex flex-col w-96" key={developer.name}>
-          <a className="rounded border-solid rounded-md border-slate-200 flex justify-center py-4 mb-4 bg-white" href={developer.href} alt="">
-            <img className="max-h-24 max-w-24" src={useBaseUrl(developer.image)} alt=""></img>
+        <li className="relative group" key={developer.name}>
+          <a className="rounded border-solid rounded-md border-slate-200 flex justify-center py-6 mb-4 bg-white" href={developer.href} alt="">
+            <img className="max-h-20 max-w-20" src={useBaseUrl(developer.image)} alt=""></img>
           </a>
           <h4 className="my-1 text-xl mt-0">{developer.name}</h4>
           <p className="text-base text-neutral-800 dark:text-neutral-300 line-clamp-3 mt-0 h-20">{developer.description}</p>
           <div className='flex items-center'>
-            <a className="mb-2 mt-1 text-base font-semibold text-red-500 flex align-center mr-1"  href="">Github Repo<img src={useBaseUrl('/img/icons/external-link.svg')} alt="" /></a>
+            <a className="mb-2 mt-1 text-base font-semibold text-red-500 flex align-center mr-2"  href="">Github Repo<img src={useBaseUrl('/img/icons/external-link.svg')} class="ml-1" alt="" /></a>
             |
-            <a className="mb-2 mt-1 text-base font-semibold text-red-500 flex align-center ml-1" href="">Twitter<img src={useBaseUrl('/img/icons/twitter-icon-blue.svg')} alt="" /></a>
+            <a className="mb-2 mt-1 text-base font-semibold text-red-500 flex align-center ml-2" href="">Twitter<img src={useBaseUrl('/img/icons/twitter-icon-blue.svg')} class="ml-2" alt="" /></a>
           </div>
         </li>
       )}
+        <li className="flex flex-col w-96">
+          <a className="rounded border-solid rounded-md border-slate-200 flex justify-center py-6 mb-4 bg-black" href="#" alt="">
+            <img className="max-h-20 max-w-20" src="/img/builtWithXmtp/building.svg" alt=""></img>
+          </a>
+          <a href="#"><h4 className="my-1 text-xl mt-0">Submit your project</h4></a>
+          <p className="text-base text-neutral-800 dark:text-neutral-300 line-clamp-3 mt-0 h-20">Have a project or company that's currently building on XMTP? <a href="#">Submit it here</a> and we'll add it to the showcase.</p>
+        </li>
     </ul>
   )
 }
