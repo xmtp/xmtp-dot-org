2124be5220feb58f7bb481304fb856a1dcfe819e
Richard Bustos <richardbustos@Richards-MacBook-Pro.local>
2022-12-15 08:44:44 -0800

    added background color back and fixed descriptions

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -11,7 +11,7 @@ const BuiltWithXmtp = () => {
     <ul className="list-none grid grid-cols-1 md:grid-cols-2 gap-6 px-0">
       {ListOfDevelopers.map(developer => 
         <li className="relative group" key={developer.name}>
-          <a className="rounded border-solid rounded-md border-slate-200 dark:border-slate-600 flex justify-center py-6 mb-3" href={developer.href}>
+          <a className="rounded border-solid rounded-md bg-white border-slate-200 dark:border-slate-600 flex justify-center py-6 mb-3" href={developer.href}>
             <img className="max-h-20 max-w-20 min-h-20" src={useBaseUrl(developer.image)} alt={"this is an image of the" + developer.name + " icon"}></img>
           </a>
           <h4 className="my-1 text-xl mt-0">{developer.name}</h4>
