1b7421ed61ac06ea9114c72a3c103da86ab1ba8b
Darick Dang <darickdang@gmail.com>
2022-12-14 22:28:49 -0800

    Spacing fix

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -11,7 +11,7 @@ const BuiltWithXmtp = () => {
     <ul className="list-none grid grid-cols-1 md:grid-cols-2 gap-6 px-0">
       {ListOfDevelopers.map(developer => 
         <li className="relative group" key={developer.name}>
-          <a className={`rounded border-solid rounded-md border-slate-200 flex justify-center py-6 mb-4 ` + developer.bgColor} href={developer.href}>
+          <a className={`rounded border-solid rounded-md border-slate-200 dark:border-slate-600 flex justify-center py-6 mb-3 ` + developer.bgColor} href={developer.href}>
             <img className="max-h-20 max-w-20 min-h-20" src={useBaseUrl(developer.image)} alt={"this is an image of the" + developer.name + " icon"}></img>
           </a>
           <h4 className="my-1 text-xl mt-0">{developer.name}</h4>
@@ -26,7 +26,7 @@ const BuiltWithXmtp = () => {
         </li>
       )}
         <li className="relative group">
-          <a className="rounded border-solid rounded-md border-slate-200 flex justify-center py-6 mb-4 bg-gradient-to-r from-indigo-900 to-red-500" href="https://forms.gle/p1VgVtkoGfHXANXt5" alt="Submit your">
+          <a className="rounded border-solid rounded-md border-slate-200 flex justify-center py-6 mb-3 bg-gradient-to-r from-indigo-900 to-red-500" href="https://forms.gle/p1VgVtkoGfHXANXt5" alt="Submit your">
             <img className="max-h-20 max-w-20 min-h-20" src="/img/builtWithXmtp/building.svg" alt=""></img>
           </a>
           <a href="https://forms.gle/p1VgVtkoGfHXANXt5"><h4 className="my-1 text-xl mt-0">Submit request</h4></a>
