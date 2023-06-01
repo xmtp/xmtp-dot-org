daddccfc2e7455df1530750a9a1ccd43f5ced7aa
fabrizioguespe <fabrizioguespe@MacFabri.local>
2023-06-01 14:02:01 -0300

    newcss

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -4,7 +4,9 @@ import TOC from '@theme-original/TOC'
 export default function TOCWrapper(props) {
   return (
     <>
-      <div className="ml-1.5 mt-2 text-sm font-bold dark:white">On this page</div>
+      <div className="ml-1.5 mt-2 text-sm font-bold dark:white">
+        On this page
+      </div>
       <TOC {...props} />
     </>
   )
