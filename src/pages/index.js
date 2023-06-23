abaf670bf43294b35bc9e6f46dcc9e41eb52dd51
Darick Dang <darickdang@gmail.com>
2023-06-23 15:00:29 -0700

    Hot fix - Jump during load of homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <header className="homepageHeader slideFade px-0 pt-40 md:pt-0 h-[600px] md:h-[770px] bg-[length:1000px_auto] md:bg-[length:1560px_auto] bg-bottom bg-no-repeat flex flex-col justify-center items-center">
+    <header className="homepageHeader px-0 pt-40 md:pt-0 h-[600px] md:h-[770px] bg-[length:1000px_auto] md:bg-[length:1560px_auto] bg-bottom bg-no-repeat flex flex-col justify-center items-center">
         
-        <div className="main-carousel">
+        <div className="main-carousel slideFade">
 
           <div className="absolute bottom-28 md:bottom-0">
             <img src="/img/leftPhone.png" className="w-[522px] leftPhone slideFade block dark:hidden"></img>
