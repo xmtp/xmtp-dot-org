60282b503d62254288fe087c4ff3390d918dcd78
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2023-07-14 18:02:47 -0700

    Revert "Homepage banner performance"

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,8 +135,22 @@ function HomepageLogos() {
 );
 }
 
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+
+  useEffect(() => {
+    
+    document.onreadystatechange = function() {
+      if (document.readyState !== "complete") {
+          document.querySelector("header.homepageHeader").style.visibility = "hidden";
+      } else {
+          document.querySelector("header.homepageHeader").style.visibility = "visible";
+      }
+  };
+  },
+
+  []);
 
   useEffect(() => {
     
