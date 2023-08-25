3c59b132b6184699e662c0933bcf82377bd7d2df
daria-github <daria@xmtp.com>
2023-08-25 08:22:30 -0700

    add mutation observer

--- src/components/Tutorial/index.js
+++ src/components/Tutorial/index.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Tutorial = () => {
-  const theme = window.localStorage.getItem("theme");
-  const param = theme.includes("dark") ? "dark" : "light";
+  const [url, setUrl] = useState(
+    "https://xmtp-alpha-interactive-tutorial.vercel.app",
+  );
+
+  useEffect(() => {
+    // Add mutation handling for the data-theme in the html. Needed so we can pass appropriate dark/light mode to iframe.
+    const handleMutations = (mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        if (
+          mutation.type === "attributes" &&
+          mutation.attributeName === "data-theme"
+        ) {
+          const theme = document.documentElement.getAttribute("data-theme");
+          const url = `https://xmtp-alpha-interactive-tutorial.vercel.app?theme=${theme}`;
+          setUrl(url);
+        }
+      }
+    };
+
+    // Create an observer instance with the callback
+    const observer = new MutationObserver(handleMutations);
+
+    // Start observing the target node for configured mutations
+    const config = {
+      attributes: true,
+      attributeOldValue: document.documentElement.getAttribute("data-theme"),
+      attributeFilter: ["data-theme"],
+    };
+    observer.observe(document.querySelector("html"), config);
+  }, []);
+
   return (
     <iframe
       allow="cross-origin-isolated"
-      src={`https://xmtp-alpha-interactive-tutorial.vercel.app?theme=${param}`}
+      src={url}
       width="100%"
       style={{ height: "100vh" }}></iframe>
   );
