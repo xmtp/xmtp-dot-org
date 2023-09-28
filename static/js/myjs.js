58b9f5b2097a86c83805f92b26632625058c51bc
fabri <fguespe@gmail.com>
2023-09-28 11:52:40 -0400

    New labels updated

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -22,25 +22,18 @@ window.onload = function () {
   // Add class to nav items based on their text
   navItems.forEach((item) => {
     if (
-      item.innerText === "Local DB" ||
-      item.innerText === "Groups" ||
       item.innerText === "Quickstart" ||
-      item.innerText === "Local-first cache" ||
       item.innerText === "Content Types" ||
       item.innerText === "Custom Content Types" ||
       item.innerText === "Get featured" ||
       item.innerText === "Launch your app" ||
       item.innerText === "Debug & Test" ||
-      item.innerText === "DAOs" ||
-      item.innerText === "DeFi" ||
-      item.innerText === "Marketing" ||
-      item.innerText === "Interactive Tutorial" ||
+      item.innerText === "Spam Filters" ||
       item.innerText === "Custom content type" ||
-      item.innerText === "Developer Quickstart" ||
       item.innerText === "Query addresses" ||
       item.innerText === "Scale" ||
       item.innerText === "Reply" ||
-      item.innerText === "Remote Attachment" ||
+      item.innerText === "Attachment" ||
       item.innerText === "Reaction" ||
       item.innerText === "Read Receipt" ||
       item.innerText === "Custom Content-Type"
