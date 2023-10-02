1d16a7ff16d511b4babd8cb00b125ef5817e42f8
fabri <fguespe@gmail.com>
2023-10-02 16:33:04 -0300

    uinbox width fix

--- src/components/UWidgets/UInbox/index.js
+++ src/components/UWidgets/UInbox/index.js
@@ -298,39 +298,26 @@ function SVGLogo({ parentClass, size, theme }) {
   const uniqueClassLogo = `logo-${Math.random().toString(36).substr(2, 9)}`;
 
   const logoStyles = {
+    container: {
+      width: "100%",
+    },
     logo: `
-    .${uniqueClassLogo} {
-      transition: transform 0.5s ease, fill 0.5s ease;
-    }
-
-    .spin-clockwise .${uniqueClassLogo} {
-      animation: spinClockwise 0.5s linear;
-    }
-
-    .spin-counter-clockwise .${uniqueClassLogo} {
-      animation: spinCounterClockwise 0.5s linear;
-    }
-    .spin-clockwise .${uniqueClassLogo} path{
-      fill: ${hoverColor};  // Add this line to change color while spinning
-    }
-
-    @keyframes spinClockwise {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
-    }
-
-    @keyframes spinCounterClockwise {
-      0% { transform: rotate(360deg); }
-      100% { transform: rotate(0deg); }
-    }
-
-    .powered .${uniqueClassLogo} {
-      width: 12px !important;
-      margin-left:2px;
-      margin-right:2px;
-      margin-top:1px;
-    }
-  `,
+        .${uniqueClassLogo} {
+          transition: transform 0.5s ease;
+        }
+        .powered .logo{
+          width:12px !important;
+          margin-left:2px;
+          margin-right:2px;
+        }
+        .${parentClass}:hover .${uniqueClassLogo} {
+          transform: rotate(360deg);
+        }
+  
+        .${parentClass}:hover .${uniqueClassLogo} path {
+          fill: ${hoverColor};
+        }
+      `,
   };
 
   return (
@@ -340,9 +327,7 @@ function SVGLogo({ parentClass, size, theme }) {
         className={"logo " + uniqueClassLogo}
         style={logoStyles.container}
         viewBox="0 0 462 462"
-        xmlns="http://www.w3.org/2000/svg"
-        width="462"
-        height="462">
+        xmlns="http://www.w3.org/2000/svg">
         <path
           fill={color}
           d="M1 231C1 103.422 104.422 0 232 0C359.495 0 458 101.5 461 230C461 271 447 305.5 412 338C382.424 365.464 332 369.5 295.003 349C268.597 333.767 248.246 301.326 231 277.5L199 326.5H130L195 229.997L132 135H203L231.5 184L259.5 135H331L266 230C266 230 297 277.5 314 296C331 314.5 362 315 382 295C403.989 273.011 408.912 255.502 409 230C409.343 131.294 330.941 52 232 52C133.141 52 53 132.141 53 231C53 329.859 133.141 410 232 410C245.674 410 258.781 408.851 271.5 406L283.5 456.5C265.401 460.558 249.778 462 232 462C104.422 462 1 358.578 1 231Z"
