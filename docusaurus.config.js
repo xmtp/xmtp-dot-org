8dfc01479965ce6164da37c1d569f641264a2e5a
daria-github <daria@xmtp.com>
2023-11-16 16:09:09 -0800

    added flickity check and fallback

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,13 +37,11 @@ const config = {
       src: "https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js",
       defer: true,
       "data-domain": "xmtp.org",
-      crossorigin: "anonymous",
     },
     {
       src: "https://unpkg.com/flickity-fade@1/flickity-fade.js",
       defer: true,
       "data-domain": "xmtp.org",
-      crossorigin: "anonymous",
     },
   ],
   clientModules: [require.resolve("./src/css/tailwind.css")],
