228b2e1cc157e1678c74e892e185a9938907a8ba
J-Ha <5481259+jhaaaa@users.noreply.github.com>
2023-11-17 18:25:44 -0800

    fix links (#602)

--- src/pages/index.js
+++ src/pages/index.js
@@ -159,7 +159,7 @@ function HomepageLogos() {
     <div class="pt-16 md:py-0">
       <div class="mx-auto max-w-[1440px] px-6 lg:px-8 block dark:hidden">
         <div class="mx-auto grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-8 sm:max-w-xl sm:grid-cols-2 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
-          <a href="/docs/tutorials/web3-social/cyberconnect">
+          <a href="/docs/use-cases/deso">
             <img
               class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="/img/logo-cyberconnect.svg"
@@ -167,7 +167,7 @@ function HomepageLogos() {
               width="206"
               height="32"></img>
           </a>
-          <a href="/docs/tutorials/web3-social/lens-multiprofile">
+          <a href="/docs/use-cases/deso">
             <img
               class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="/img/logo-lens.svg"
