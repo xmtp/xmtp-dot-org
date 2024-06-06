db3dc71b59459e5a09c02eb48871ef1c2cc44a12
Darick Dang <darickdang@gmail.com>
2024-06-05 21:32:21 -0700

    Dark mode fixes

--- src/components/NotifiCaseStudy/index.js
+++ src/components/NotifiCaseStudy/index.js
@@ -5,19 +5,19 @@ const NotifiCaseStudy = () => {
   return (
   <div>
     <div class="relative isolate overflow-hidden">
-    <div class="absolute inset-0 -z-10 overflow-hidden">
-      <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
-        <defs>
-          <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-            <path d="M100 200V.5M.5 .5H200" fill="none" />
-          </pattern>
-        </defs>
-        <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
-          <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+      <div class="absolute inset-0 -z-10 overflow-hidden">
+        <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 dark:stroke-gray-800 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+          <defs>
+            <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y="-1" class="overflow-visible fill-gray-50 dark:fill-gray-800">
+            <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+          </svg>
+          <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
         </svg>
-        <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-      </svg>
-    </div>
+      </div>
       <div class="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]" aria-hidden="true">
       </div>
       <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-10">
@@ -27,12 +27,12 @@ const NotifiCaseStudy = () => {
               <span class="rounded-full bg-red-500/10 px-3 py-1 text-sm font-semibold leading-6 text-red-400 ring-1 ring-inset ring-red-500/20">Case Study</span>
             </a>
           </div>
-          <h1 class="mt-6 text-4xl font-bold tracking-tight text-black sm:text-6xl">Notifi uses XMTP to deliver real-time alerts on Coinbase Wallet</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-500">Notifi’s integration with XMTP enables DeFi projects like GMX to deliver real-time notifications and critical alerts for traders directly in Coinbase Wallet.</p>
+          <h1 class="mt-6 text-4xl font-bold tracking-tight sm:text-6xl">Notifi uses XMTP to deliver real-time alerts on Coinbase Wallet</h1>
+          <p class="mt-6 text-lg leading-8">Notifi’s integration with XMTP enables DeFi projects like GMX to deliver real-time notifications and critical alerts for traders directly in Coinbase Wallet.</p>
           <div class="mt-6 flex items-center gap-x-6">
             <a href="/docs/tutorials/portable-consent/subscribe" class="rounded-md bg-red-500 px-6 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-red-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400 pointer hover:no-underline">Get started</a>
           </div>
-          <div class="bg-white py-12">
+          <div class="py-12">
             <div class="mx-auto max-w-7xl">
               <div class="mx-auto grid grid-cols-3 gap-x-4 gap-y-10">
                 <img class="col-span-1 max-h-12 w-full object-contain lg:col-span-1" src="img/cbwalletLogo.svg" alt="Coinbase Wallet" width="283" height="48" />
@@ -50,29 +50,29 @@ const NotifiCaseStudy = () => {
       </div>
     </div>
 
-    <div class="bg-white py-16">
+    <div class="py-16">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-3xl lg:text-center">
           <h2 class="text-base font-semibold leading-7 text-red-600 uppercase">Overview</h2>
-          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Helping GMX users trade better</p>
-          <p class="mt-6 text-xl leading-8 text-gray-600">GMX is a decentralized exchange focused on spot and perpetual trading on the Arbitrum blockchain. It is the first DEX to partner with Notifi to deliver timely and relevant alerts directly through Coinbase Wallet, reaching over 530,000+ traders in their main inbox. The integration streamlines communication, simplifies engagement with the platform, and ultimately empowers users to make better trading decisions.</p>
+          <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Helping GMX users trade better</p>
+          <p class="mt-6 text-xl leading-8">GMX is a decentralized exchange focused on spot and perpetual trading on the Arbitrum blockchain. It is the first DEX to partner with Notifi to deliver timely and relevant alerts directly through Coinbase Wallet, reaching over 530,000+ traders in their main inbox. The integration streamlines communication, simplifies engagement with the platform, and ultimately empowers users to make better trading decisions.</p>
         </div>
       </div>
     </div>
 
-    <div class="overflow-hidden bg-white py-16 sm:py-16">
+    <div class="overflow-hidden py-16 sm:py-16">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start">
           <div class="lg:pr-4 lg:pt-4">
             <div class="lg:max-w-lg">
               <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-red-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
                 </svg>
               </div>
-              <p class="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Let users subscribe from anywhere</p>
-              <p class="mt-6 text-lg leading-7 text-gray-600">By building on top of XMTP’s consent standards, Notifi makes it possible for users to opt-in to messages from GMX via subscribe buttons across gmx.com, apps, or Frames on Farcaster.</p>
-              <p class="mt-6 text-lg leading-7 text-gray-600">When a user subscribes, they are adding GMX’s unique address to their allowed contacts list, which is controlled by the user and accessible by any XMTP client app once they log in. This gives Coinbase Wallet a reliable source of truth to respect users’ contact preferences and keep users’ main inbox free from spam.</p>
+              <p class="mt-6 text-3xl font-bold tracking-tight sm:text-4xl">Let users subscribe from anywhere</p>
+              <p class="mt-6 text-lg leading-7">By building on top of XMTP’s consent standards, Notifi makes it possible for users to opt-in to messages from GMX via subscribe buttons across gmx.com, apps, or Frames on Farcaster.</p>
+              <p class="mt-6 text-lg leading-7">When a user subscribes, they are adding GMX’s unique address to their allowed contacts list, which is controlled by the user and accessible by any XMTP client app once they log in. This gives Coinbase Wallet a reliable source of truth to respect users’ contact preferences and keep users’ main inbox free from spam.</p>
               <div class="mt-8">
                 <a href="/docs/tutorials/portable-consent/subscribe" class="inline-flex rounded-md bg-red-600 px-6 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-red-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 pointer hover:no-underline">Get started</a>
               </div>
@@ -83,22 +83,22 @@ const NotifiCaseStudy = () => {
       </div>
     </div>
 
-    <div class="overflow-hidden bg-white py-16 sm:py-16">
+    <div class="overflow-hidden py-16 sm:py-16">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div class="lg:ml-auto lg:pl-4 lg:pt-4">
             <div class="lg:max-w-lg">
             <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-red-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
                 </svg>
               </div>
-              <p class="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Deliver real-time notifications </p>
-              <p class="mt-6 text-lg leading-7 text-gray-600">Via Notifi, publishers can segment and send broadcast messages to their list of subscribers. Since Coinbase Wallet respects users’ opt-in preferences stored on the XMTP network, publishers can feel confident that their messages will reach subscribers’ main inbox.</p>
-              <p class="mt-6 text-lg leading-7 text-gray-600">Starting today, GMX users can receive key notifications, including:</p>
-              <dl class="pl-4 my-8 max-w-xl space-y-6 text-base leading-7 text-gray-600 lg:max-w-none">
+              <p class="mt-6 text-3xl font-bold tracking-tight sm:text-4xl">Deliver real-time notifications </p>
+              <p class="mt-6 text-lg leading-7">Via Notifi, publishers can segment and send broadcast messages to their list of subscribers. Since Coinbase Wallet respects users’ opt-in preferences stored on the XMTP network, publishers can feel confident that their messages will reach subscribers’ main inbox.</p>
+              <p class="mt-6 text-lg leading-7">Starting today, GMX users can receive key notifications, including:</p>
+              <dl class="pl-4 my-8 max-w-xl space-y-6 text-base leading-7 lg:max-w-none">
                 <div class="relative pl-12">
-                  <dt class="inline font-semibold text-gray-900 text-lg">
+                  <dt class="inline font-semibold text-lg">
                   <svg class="absolute left-1 top-0.5 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                   </svg>
@@ -106,7 +106,7 @@ const NotifiCaseStudy = () => {
                   </dt>
                 </div>
                 <div class="relative pl-12">
-                  <dt class="inline font-semibold text-gray-900 text-lg">
+                  <dt class="inline font-semibold text-lg">
                   <svg class="absolute left-1 top-0.5 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
                   </svg>
@@ -114,7 +114,7 @@ const NotifiCaseStudy = () => {
                   </dt>
                 </div>
                 <div class="relative pl-12">
-                  <dt class="inline font-semibold text-gray-900 text-lg">
+                  <dt class="inline font-semibold text-lg">
                   <svg class="absolute left-1 top-0.5 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
                   </svg>
@@ -138,18 +138,18 @@ const NotifiCaseStudy = () => {
       </div>
     </div>
 
-    <div class="overflow-hidden bg-white py-16 sm:py-16">
+    <div class="overflow-hidden py-16 sm:py-16">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center">
           <div class="lg:pr-4 lg:pt-4">
             <div class="lg:max-w-lg">
               <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-red-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z" />
                 </svg>
               </div>
-              <p class="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Build subscriber lists without collecting personal data</p>
-              <p class="mt-6 text-lg leading-7 text-gray-600">By default, subscriptions on XMTP are encrypted and private. By integrating XMTP’s subscribe and broadcast functionality, Notifi can build distribution lists for clients like GMX, while keeping their end-users identities and subscription preferences private. This means that GMX can reach their users through more secure inboxes, like Coinbase Wallet, while users never have to worry about their subscription data being shared with third parties.</p>
+              <p class="mt-6 text-3xl font-bold tracking-tight sm:text-4xl">Build subscriber lists without collecting personal data</p>
+              <p class="mt-6 text-lg leading-7">By default, subscriptions on XMTP are encrypted and private. By integrating XMTP’s subscribe and broadcast functionality, Notifi can build distribution lists for clients like GMX, while keeping their end-users identities and subscription preferences private. This means that GMX can reach their users through more secure inboxes, like Coinbase Wallet, while users never have to worry about their subscription data being shared with third parties.</p>
               <div class="mt-8">
                 <a href="/docs/tutorials/portable-consent/subscribe" class="inline-flex rounded-md bg-red-600 px-6 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-red-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 pointer hover:no-underline">Get started</a>
               </div>
