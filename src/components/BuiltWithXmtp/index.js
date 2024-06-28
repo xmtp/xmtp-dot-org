d19d2607d00d3ffc7c0ff045b9db070e16ec42dd
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2024-06-28 11:48:13 -0700

    update stat on home page

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -24,7 +24,7 @@ const BuiltWithXmtp = () => {
             <p className="text-lg leading-7 text-gray-600">
               <strong className="pr-1">
                 <a href="https://dune.com/xmtp_team/dash" target="_blank" rel="noreferrer">
-                  1,977,931+
+                  1,991,140+
                 </a>
               </strong>
               web3 identities rely on apps built with XMTP for secure, private,
