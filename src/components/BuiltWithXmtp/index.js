0b142131f555cf7e21ecd9c81d9f467839a95c50
Darick Dang <darickdang@gmail.com>
2024-12-05 12:12:32 -0800

    Copy updates

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -106,7 +106,7 @@ const BuiltWithXmtp = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 me-2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
                   </svg>
-                  Try XMTP Now<span aria-hidden="true">→</span>
+                  Try XMTP now<span aria-hidden="true">→</span>
                 </a>
                 
               </div>
@@ -209,7 +209,7 @@ const BuiltWithXmtp = () => {
               <div class="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none text-center md:text-left">
                   <img src="img/converse-logo-full-black.svg" class="h-14 mb-2"></img>
                   <h2 class="max-w-3xl text-balance text-3xl md:text-4xl font-semibold tracking-tight text-gray-950">Want to try XMTP messaging?</h2>
-                  <p class="max-w-3xl text-base md:text-lg font-normal text-gray-600">Converse Messenger is a messaging app for decentralized, secure, end-to-end encrypted conversations.</p>
+                  <p class="max-w-3xl text-base md:text-lg font-normal text-gray-600">Converse Messenger is an app for decentralized, secure, end-to-end encrypted conversations.</p>
                   <div class="mt-6 flex flex-wrap gap-x-4 gap-y-0 justify-center md:justify-normal">
                     <a aria-label="Download on the App Store" href="https://apps.apple.com/app/converse-messenger/id1658819514">
                         <svg width="130" height="44" viewBox="0 0 180 52" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -581,9 +581,9 @@ const BuiltWithXmtp = () => {
               <div class="relative isolate overflow-hidden bg-[#1d1d1d] px-6 pt-8 pb-8 md:pt-16 md:pb-16 text-left shadow-2xl sm:rounded-2xl sm:px-16 grid grid-cols-1 md:grid-cols-2 gap-x-8 text-center">
                 
                 <div>
-                  <h2 class="text-balance text-4xl font-semibold tracking-tight text-white sm:text-4xl">XMTP is Open Source</h2>
+                  <h2 class="text-balance text-4xl font-semibold tracking-tight text-white sm:text-4xl">XMTP is open source</h2>
                   <p class="mt-2 max-w-2xl text-pretty text-md/8 md:text-lg/7 text-gray-300">XMTP’s protocol, app, and developer tools are all open source, enabling transparency, security, and community-driven improvements. XMTP’s features into their applications, expanding the ecosystem while upholding decentralized and privacy-first standards.</p>
-
+{/* 
                   <div class="relative h-auto text-gray-200 text-left mt-6 mb-4">
                     <div class=" bottom-0 left-20 right-20 top-16 overflow-hidden rounded-xl bg-gray-950">
                       <code class="text-xs md:text-base inline-flex text-left items-center space-x-4 bg-gray-950 text-white p-6 w-full">
@@ -604,7 +604,7 @@ const BuiltWithXmtp = () => {
                         </span>
                       </code>
                     </div>
-                  </div>
+                  </div> */}
                   
                   <div class="mt-10 flex gap-x-6 mb-8 justify-center">
                   
@@ -612,7 +612,7 @@ const BuiltWithXmtp = () => {
                     
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 me-2" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
 
-                    Browse all of the Repos <span class="ml-2" aria-hidden="true">→</span>
+                    Browse XMTP repos <span class="ml-2" aria-hidden="true">→</span>
                     </a>
                     
                   </div>  
@@ -621,7 +621,7 @@ const BuiltWithXmtp = () => {
                 <div>
                   <div>
                     
-                    <a href="https://github.com/xmtp/xmtp-js" target="_blank" class="hover:no-underline">
+                    <a href="https://github.com/xmtp/xmtp-js/tree/main/sdks/browser-sdk" target="_blank" class="hover:no-underline">
                       <button 
                         className="cursor-pointer w-full flex justify-between items-center py-5 text-white bg-transparent text-lg border-none md:border-solid border-0 border-b border-gray-600 hover:opacity-75"
                       >
@@ -655,24 +655,6 @@ const BuiltWithXmtp = () => {
 
                   <div>
                     
-                    <a href="https://github.com/xmtp/xmtp-ios" target="_blank" class="hover:no-underline">
-                      <button 
-                        className="cursor-pointer w-full flex justify-between items-center py-5 text-white bg-transparent text-lg border-none md:border-solid border-0 border-b border-gray-600 hover:opacity-75"
-                      >
-                      <div class="flex gap-x-6">
-                        <img class="max-h-8 object-contain" src="img/swiftLogo.png" alt="Swift Logo"></img>
-                        <span class="text-lg md:text-xl font-semibold">Swift (iOS)</span>
-                      </div>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                      </svg>
-                    </button>
-                    </a>
-
-                  </div>
-
-                  <div>
-                    
                     <a href="https://github.com/xmtp/xmtp-android" target="_blank" class="hover:no-underline">
                       <button 
                         className="cursor-pointer w-full flex justify-between items-center py-5 text-white bg-transparent text-lg border-none md:border-solid border-0 border-b border-gray-600 hover:opacity-75"
@@ -691,7 +673,25 @@ const BuiltWithXmtp = () => {
 
                   <div>
                     
-                    <a href="https://github.com/xmtp/xmtpd" target="_blank" class="hover:no-underline">
+                    <a href="https://github.com/xmtp/xmtp-ios" target="_blank" class="hover:no-underline">
+                      <button 
+                        className="cursor-pointer w-full flex justify-between items-center py-5 text-white bg-transparent text-lg border-none md:border-solid border-0 border-b border-gray-600 hover:opacity-75"
+                      >
+                      <div class="flex gap-x-6">
+                        <img class="max-h-8 object-contain" src="img/swiftLogo.png" alt="Swift Logo"></img>
+                        <span class="text-lg md:text-xl font-semibold">Swift (iOS)</span>
+                      </div>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                      </svg>
+                    </button>
+                    </a>
+
+                  </div>
+
+                  <div>
+                    
+                    <a href="https://github.com/xmtp/xmtp-js/tree/main/sdks/node-sdk" target="_blank" class="hover:no-underline">
                       <button 
                         className="cursor-pointer w-full flex justify-between items-center py-5 text-white bg-transparent text-lg border-none md:border-solid border-0 border-b border-gray-600 hover:opacity-75"
                       >
