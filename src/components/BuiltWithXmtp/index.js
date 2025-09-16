474ce9ab010100cf5b8869c52ab17fb261cc7a4c
J-Ha <5481259+jhaaaa@users.noreply.github.com>
2025-09-16 13:06:04 -0700

    Update consent product marketing page (#851)
    * first pass
    
    * edits from peter <3

--- src/components/BuiltWithXmtp/index.js
+++ src/components/BuiltWithXmtp/index.js
@@ -494,7 +494,7 @@ const BuiltWithXmtp = () => {
               <div class="lg:max-w-lg">
                 <h2 class="text-base/7 font-semibold text-red-500">Consent</h2>
                 <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight">Built-in spam protection</p>
-                <p class="mt-6 text-md/8 md:text-lg/8">Users block spam once, and it's blocked everywhere they go. Your app automatically inherits spam protection that works across the entire XMTP network — giving every user control of who can message them.</p>
+                <p class="mt-6 text-md/8 md:text-lg/8">Users block spam once, and it's blocked everywhere they go. Your app automatically inherits spam protection that works across the entire XMTP network — giving every user control of who can reach their inbox.</p>
                 <p class="mt-12">
 
                   <a href="/vision/concepts/consent" class="text-md/8 md:text-lg/8 cursor-pointer items-center gap-x-1 text-white bg-gray-900 hover:text-white shadow-sm hover:bg-red-600 transition-all font-semibold rounded-md text-base text-center inline-flex items-center me-2 px-5 py-3.5 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400 w-full md:w-auto justify-center md:justify-normal hover:no-underline">
