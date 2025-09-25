bd3e360c22793b82a93b2a943873f86bb25dfc50
Darick Dang <darickdang@gmail.com>
2025-09-25 16:25:57 -0700

    zero-config setup text update

--- src/components/Agents/index.js
+++ src/components/Agents/index.js
@@ -429,7 +429,7 @@ const Agents = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <span className="text-gray-200 font-semibold text-base/6">One environment variable</span>
+                <span className="text-gray-200 font-semibold text-base/6">Zero-config setup</span>
               </div>
               
               <div className="flex items-center space-x-2">
